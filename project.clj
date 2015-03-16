b7370c8fcf8389d7b85f6606ad26bd97dff275f1
Sheel Choksi <sheelchoksi@gmail.com>
2015-03-16 10:17:01 -0700

    Initial commit
    Adding the kafka component from ship-monkey for all to share

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject kafka-component "0.1.0-SNAPSHOT"
+  :description "A kafka component to consume from Kafka"
+  :url "https://github.com/Mayvenn/kafka-component"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[s3-wagon-private "1.1.2"]]
+  
+                             
+                             
+                             
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.stuartsierra/component "0.2.2"]
+                 [clj-kafka "0.2.8-0.8.1.1"]])
---
+++ src/kafka_component/core.clj
@@ -0,0 +1,42 @@
+(ns kafka-component.core
+  (:require [com.stuartsierra.component :as component]
+            [clj-kafka.consumer.zk :refer [consumer shutdown messages]])
+  (:import [java.util.concurrent Executors TimeUnit]))
+
+(defn consume-messages-task
+  [logger exception-handler message-consumer thread-id messages kafka-consumer]
+  (fn []
+    (logger :info (str "consumer thread " thread-id " starting"))
+    (doseq [m messages]
+      (try
+        (do
+          (logger :info (str "thread " thread-id " received message with key: " (String. (:key m))))
+          (message-consumer m)
+          (.commitOffsets kafka-consumer))
+        (catch Exception e
+          (do (logger :error (str "error in consumer thread " thread-id))
+              (logger :error e)
+              (exception-handler e)))))))
+
+(defrecord KafkaConsumerPool [config pool-size topic consumer-component logger exception-handler]
+  component/Lifecycle
+  (start [c]
+    (logger :info (str "starting " topic " consumption"))
+    (let [thread-pool (Executors/newFixedThreadPool pool-size)
+          kafka-consumers (repeatedly pool-size #(consumer (config :kafka-consumer-config)))
+          message-lists (map #(messages % topic) kafka-consumers)
+          tasks (map (partial consume-messages-task logger exception-handler (:consumer consumer-component))
+                     (map (partial str topic "-") (range))
+                     message-lists
+                     kafka-consumers)]
+      (doseq [t tasks] (.submit thread-pool t))
+      (logger :info (str "started " topic " consumption"))
+      (merge c {:thread-pool thread-pool :kafka-consumers kafka-consumers})))
+  (stop [{:keys [logger thread-pool kafka-consumers] :or {logger :noop} :as c}]
+    (logger :info (str "stopping " topic " consumption"))
+    (when thread-pool
+      (.shutdown thread-pool)
+      (.awaitTermination thread-pool (config :shutdown-grace-period) TimeUnit/SECONDS))
+    (doseq [consumer kafka-consumers] (.shutdown consumer))
+    (logger :info (str "stoppped " topic " consumption"))
+    c))
