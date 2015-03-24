86e98f6805a80153163959e059a20dd2df3cd3c1
Sheel Choksi <sheelchoksi@gmail.com>
2015-03-24 16:25:30 -0700

    Renaming private repository to releases

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[s3-wagon-private "1.1.2"]]
-  
-                             
-                             
-                             
+  :repositories [["releases" {:url "s3p://mayvenn-dependencies/releases/"
+                              
+                              
+                              
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.stuartsierra/component "0.2.2"]
                  [clj-kafka "0.2.8-0.8.1.1"]])
