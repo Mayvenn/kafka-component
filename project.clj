d349b2c05e24c99749f60c7a0de03ed8981f04db
Sheel Choksi <sheelchoksi@gmail.com>
2016-06-28 13:43:43 -0700

    Add missing deploy repository

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/Mayvenn/kafka-component"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.stuartsierra/component "0.2.2"]
                  [clj-kafka "0.3.2"]])
