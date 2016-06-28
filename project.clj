4aab10e6d2c97413b73916034ad3286778c3e252
Sheel Choksi <sheelchoksi@gmail.com>
2016-06-28 13:12:08 -0700

    Bump to 0.2.2 for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kafka-component "0.2.2-SNAPSHOT"
+(defproject kafka-component "0.2.2"
   :description "A kafka component to consume from Kafka"
   :url "https://github.com/Mayvenn/kafka-component"
   :license {:name "Eclipse Public License"
