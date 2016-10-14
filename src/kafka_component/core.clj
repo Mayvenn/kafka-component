1de49ad9f07fbc1e1506fe06ed1735d0d99666c5
Erika Yoon <erika@mayvenn.com>
2016-10-14 10:34:53 -0700

    Give ownership of kafka consumer to task

--- src/kafka_component/core.clj
+++ src/kafka_component/core.clj
@@ -25,13 +25,14 @@
   (gregor/producer (overrides "bootstrap.servers")
                    (merge config/default-producer-config overrides)))
 
-(defn make-task [logger exception-handler process-record kafka-consumer task-id]
-  (letfn [(log-exception [e & msg]
-            (logger :error (apply str "task-id=" task-id " " msg))
-            (logger :error e)
-            (exception-handler e))
-          (log [& msg]
-               (logger :info (apply str "task-id=" task-id " " msg)))]
+(defn make-task [logger exception-handler process-record make-kafka-consumer task-id]
+  (let [kafka-consumer (make-kafka-consumer)
+        log-exception  (fn log-exception [e & msg]
+                         (logger :error (apply str "task-id=" task-id " " msg))
+                         (logger :error e)
+                         (exception-handler e))
+        log            (fn log [& msg]
+                         (logger :info (apply str "task-id=" task-id " " msg)))]
     (reify
       java.lang.Runnable
       (run [_]
@@ -58,10 +59,10 @@
       (close [_]
         (gregor/wakeup kafka-consumer)))))
 
-(defn init-and-start-task-pool [make-native-consumer make-task pool-id concurrency-level]
+(defn init-and-start-task-pool [make-task pool-id concurrency-level]
   (let [native-pool (Executors/newFixedThreadPool concurrency-level)
         task-ids    (map (partial str pool-id "-") (range concurrency-level))
-        tasks       (map (fn [task-id] (make-task (make-native-consumer) task-id)) task-ids)]
+        tasks       (map make-task task-ids)]
     (doseq [t tasks] (.submit native-pool t))
     {:native-pool native-pool
      :tasks       tasks}))
@@ -78,12 +79,12 @@
     (assert (not= shutdown-timeout 0) "\"shutdown-timeout\" must not be zero")
     (assert (ifn? (:process record-processor)) "record-processor does not have a function :process")
     (let [make-native-consumer (partial make-consumer native-consumer-type topics native-consumer-overrides) ; a thunk, does not need more args
-          make-task            (partial make-task logger exception-handler (:process record-processor))
-                                        ;will get native-consumer and task-id when pool is started
+          make-task            (partial make-task logger exception-handler (:process record-processor) make-native-consumer)
+                                        ;will get task-id when pool is started
           pool-id              (pr-str topics)
           log-action           (fn [& msg] (logger :info (apply str "pool-id=" pool-id " action=" msg)))]
       (log-action "starting-consumption concurrency-level=" concurrency-level " shutdown-timeout=" shutdown-timeout " topics=" topics)
-      (let [running-pool (init-and-start-task-pool make-native-consumer make-task pool-id concurrency-level)]
+      (let [running-pool (init-and-start-task-pool make-task pool-id concurrency-level)]
         (log-action "started-consumption")
         (merge this
                {:pool       running-pool
