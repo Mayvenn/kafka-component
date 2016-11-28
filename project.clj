a065abd66726e83742be0bcc149da99e7d9ad203
Jeff Hui <jeff@mayvenn.com>
2016-11-28 09:40:00 -0800

    Version 0.5.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kafka-component "0.5.7"
+(defproject kafka-component "0.5.8-SNAPSHOT"
   :description "A kafka component to consume from Kafka"
   :url "https://github.com/Mayvenn/kafka-component"
   :license {:name "Eclipse Public License"
