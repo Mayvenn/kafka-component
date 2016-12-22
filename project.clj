cd74126f323bbe98c9a0f4a2212a4377fda0d09c
Jacob Maine <jacob@mayvenn.com>
2016-12-22 11:19:23 -0800

    Version 0.5.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kafka-component "0.5.8-SNAPSHOT"
+(defproject kafka-component "0.5.8"
   :description "A kafka component to consume from Kafka"
   :url "https://github.com/Mayvenn/kafka-component"
   :license {:name "Eclipse Public License"
