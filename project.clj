759b6f58a5ab73a10dcd697e42c36749f30d0072
Jeff Hui <jeff@mayvenn.com>
2018-01-10 11:03:15 -0800

    Update dependencies
    - Clojure 1.8 -> 1.9
    - gregor 0.5.1 -> 0.6.0
    - core.async 0.2.391 -> 0.4.474

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.stuartsierra/component "0.2.2"]
-                 [io.weft/gregor "0.5.1"]
-                 [org.clojure/core.async "0.2.391"]]
+                 [io.weft/gregor "0.6.0"]
+                 [org.clojure/core.async "0.4.474"]]
   :codox {:source-paths ["src"]
           :source-uri "http://github.com/Mayvenn/kafka-component/blob/master/{filepath}#L{line}"
           :metadata {:doc/format :markdown}
