f202d8e3fb72c6a291b9d628f70765a8fd9c96ec
Jeff Hui <jeff@mayvenn.com>
2019-08-21 11:00:13 -0700

    Bump to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kafka-component "0.7.0"
+(defproject kafka-component "0.7.1-SNAPSHOT"
   :description "A kafka component to consume from Kafka"
   :url "https://github.com/Mayvenn/kafka-component"
   :license {:name "Eclipse Public License"
